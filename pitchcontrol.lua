784dbafcd26455dde22175a5668c621defc14cab
FichteFoll <fichtefoll2@googlemail.com>
2017-02-14 21:52:53 +0100

    Change license of pitchcontrol to ISC
    Specifically, remove repo-global "LICENSE" file.

--- pitchcontrol.lua
+++ pitchcontrol.lua
@@ -1,18 +1,35 @@
 -- pitchcontrol.lua
--- Version: 0.2.0
 --
--- Author: FichteFoll
+-- Version: 0.2.0
 -- URL: https://github.com/FichteFoll/mpv-scripts
--- License: MIT
+-- License: ISC
 
--- script-message-to pitchcontrol increase
---   Increase pitch by one half-tone. Bound to Alt+R.
--- script-message-to pitchcontrol decrease
---   Increase pitch by one half-tone. Bound to Alt+r.
--- script-message-to pitchcontrol toggle
---   Toggle pitching.
--- script-message-to pitchcontrol set_halftone_pitch <number>
---   Sets pitch to <number> half-tones - positive, negative or 0.
+-- Commands for key bindings:
+--
+-- * script-message-to pitchcontrol increase
+--     Increase pitch by one half-tone. Bound to Alt+R.
+-- * script-message-to pitchcontrol decrease
+--     Increase pitch by one half-tone. Bound to Alt+r.
+-- * script-message-to pitchcontrol toggle
+--     Toggle pitching.
+-- * script-message-to pitchcontrol set_halftone_pitch <number>
+--     Sets pitch to <number> half-tones - positive, negative or 0.
+
+
+-- Copyright 2016-2017 FichteFoll
+--
+-- Permission to use, copy, modify, and/or distribute this software for any
+-- purpose with or without fee is hereby granted, provided that the above
+-- copyright notice and this permission notice appear in all copies.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+-- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+-- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+-- SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+-- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+-- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+-- IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 
 local utils = require 'mp.utils'
 local msg = require('mp.msg')
